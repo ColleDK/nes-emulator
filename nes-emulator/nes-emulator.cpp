0ec937d37a556a5eeb37c5288b4b7e709e973a2f
ColleDK <s195490@student.dtu.dk>
2023-01-21 16:32:09 +0100

    Add project files.

---
+++ nes-emulator/nes-emulator.cpp
@@ -0,0 +1,12 @@
+﻿// nes-emulator.cpp : Defines the entry point for the application.
+//
+
+#include "nes-emulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
